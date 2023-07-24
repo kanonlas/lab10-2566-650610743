9c313dc88a1f1e292cb33044ec61318bc0206078
lab 23 <kanonlas_r@cmu.ac.th>
2023-07-24 15:37:39 +0700

    edited

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
-
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
 import { useState } from "react";
 
@@ -7,7 +8,7 @@ export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [genAmount, setGenAmount] = useState(1);
+  const [genAmount, setGenAmount] = useState(1); // อยากได้กี่คน
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -15,7 +16,9 @@ export default function RandomUserPage() {
       `https://randomuser.me/api/?results=${genAmount}`
     );
     setIsLoading(false);
-    const users = resp.data.results;
+    const users = resp.data.results.map((n) => cleanUser(n)); //user เป็น array
+    setUsers(users);
+
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
@@ -40,7 +43,16 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((n) => (
+          <UserCard
+            name={n.name}
+            imgUrl={n.imgUrl}
+            address={n.address}
+            email={n.email}
+          ></UserCard>
+        ))}
     </div>
   );
 }
